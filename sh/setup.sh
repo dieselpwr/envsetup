06f1cfd879c7ce4804356dc583509eb78d78161b
dieselpwr <34730627+dieselpwr@users.noreply.github.com>
2019-12-04 09:16:21 -0600

    Update setup.sh

--- sh/setup.sh
+++ sh/setup.sh
@@ -5,38 +5,28 @@ sudo apt update
 sudo apt --assume-yes upgrade
 
 # vscode install
-sudo apt update
-sudo apt --assume-yes install curl apt-transport-https
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt update
-sudo apt --assume-yes install code
-# manually install these extensions: markdownlint, Python, SQLite
+# install Visual Studio Code from Ubuntu Software
+# manually install the Python extension
 
 # git install and setup
 sudo apt --assume-yes install git
 git config --global user.name "dieselpwr"
 git config --global user.email "dieselpwr99@gmail.com"
-echo "*.pyc
-*.json
-*.sqlite3
-venv/" > ~/.gitignore
+echo "*.pyc" > ~/.gitignore
 git config --global core.excludesfile "~/.gitignore"
 
 # python install
-sudo apt --assume-yes install python3.7
+sudo apt --assume-yes install python3.8
 sudo apt --assume-yes install python3-venv # weird but needed
-sudo apt --assume-yes install python3.7-venv
+sudo apt --assume-yes install python3.8-venv
+python -m pip install pip --upgrade
+python -m pip install setuptools --upgrade
 mkdir ~/SOURCE_DRIVE
-python3.7 -m venv ~/SOURCE_DRIVE/venv
+python3.8 -m venv ~/SOURCE_DRIVE/venv
 source ~/SOURCE_DRIVE/venv/bin/activate
 python -m pip install pip --upgrade
 python -m pip install setuptools --upgrade
-python -m pip install wheel --upgrade
 python -m pip install pylint --upgrade
 python -m pip install autopep8 --upgrade
-python -m pip install pipreqs --upgrade
 python -m pip install Django -- upgrade
 python -m pip install awsebcli --upgrade
-python -m pip install django-tz-detect -- upgrade
