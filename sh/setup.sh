4db9128bdd1f2f49d843a3b264d68b382eeadddb
dieselpwr <dieselpwr99@gmail.com>
2019-12-04 10:17:28 -0600

    simplified and updated

--- sh/setup.sh
+++ sh/setup.sh
@@ -4,10 +4,6 @@
 sudo apt update
 sudo apt --assume-yes upgrade
 
-# vscode install
-# install Visual Studio Code from Ubuntu Software
-# manually install the Python extension
-
 # git install and setup
 sudo apt --assume-yes install git
 git config --global user.name "dieselpwr"
@@ -17,14 +13,15 @@ git config --global core.excludesfile "~/.gitignore"
 
 # python install
 sudo apt --assume-yes install python3.8
-sudo apt --assume-yes install python3-venv # weird but needed
-sudo apt --assume-yes install python3.8-venv
+python3.8 -m pip install pip --upgrade
+python3.8 -m pip install setuptools --upgrade
+python3.8 -m pip install pylint --upgrade
+python3.8 -m pip install autopep8 --upgrade
+python3.8 -m pip install Django -- upgrade
+python3.8 -m pip install awsebcli --upgrade
+
+# vscode install
 mkdir ~/SOURCE_DRIVE
-python3.8 -m venv ~/SOURCE_DRIVE/venv
-source ~/SOURCE_DRIVE/venv/bin/activate
-python -m pip install pip --upgrade
-python -m pip install setuptools --upgrade
-python -m pip install pylint --upgrade
-python -m pip install autopep8 --upgrade
-python -m pip install Django -- upgrade
-python -m pip install awsebcli --upgrade
+# manually install Visual Studio Code from Ubuntu Software
+# manually install the Python extension
+# manually add SOURCE_DRIVE to workspace
